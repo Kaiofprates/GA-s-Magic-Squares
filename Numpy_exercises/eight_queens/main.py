373a6595fbd5e625c72c60b272e552f8dbfbf65f
Kaiofprates <kaioprates@outlook.com>
2020-12-19 19:39:51 -0300

    improve mating pool

--- Numpy_exercises/eight_queens/main.py
+++ Numpy_exercises/eight_queens/main.py
@@ -2,34 +2,48 @@ from dna import Dna
 import numpy as np
 from numpy import random
 
-gene  = Dna(population=100,mutation=0.05)
+gene  = Dna(population=200,mutation=0.05)
 
-population  = gene.randomic_population()
+pop = gene.randomic_population()
 
+generation = 1
 
-def epoch():
-
+def epoch(population):
+    global generation
+    print("""------- Generation -------""" + str(generation))
+    generation = generation + 1
     for i in range(len(population)):
         # Fit
         population[i].fitness()
         # mating Pool
 
-    matingPool = []
+    matingPool = [gene]
+    new_population = []
 
     for i in range(len(population)):
         n = population[i].fit * 100
-
         for j in range(n):
             matingPool.append(population[i])
-            father = matingPool[random.choice(len(matingPool) )]
-            mother = matingPool[random.choice(len(matingPool) )]
 
+    for i in range(len(population)):
+        father = matingPool[random.choice(len(matingPool) )]
+        mother = matingPool[random.choice(len(matingPool) )]
+        if(len(father.genes) == 8 and len(mother.genes) == 8):
             child = gene.cross_over(father.genes,mother.genes)
             child.fitness()
-            print(child.fit)
+            print('------ error {}'.format(child.fit))
+            if child.fit == 0:
+                print(child.genes)
+                return 0
+            new_population.append(child)
+
+    epoch(new_population)
+
+
+
 
 def main():
-    epoch()
+    epoch(pop)
 
 
 if __name__ == "__main__":
